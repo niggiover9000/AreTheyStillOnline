1dd0069d1c76690ac95456a50a4a9deb6db24614
niggiover9000 <nicolasbartels@web.de>
2020-11-20 15:40:19 +0100

    Updated to rebuild

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from socket import gethostbyname, gethostname
 from multiprocessing.pool import ThreadPool
 from time import sleep
 from load_settings import LoadSettings
+from gui import RenderWindow
 
 
 class Ping:
     def __init__(self):
+        self.window = RenderWindow()
         loading = LoadSettings()
         loading.open_settings("IPs", "timeout", 2)
         self.timeout = loading.load_settings()
-        loading.open_settings("IPs", "ips", ["8.8.8.8"])
-        self.ips = loading.load_settings()
         self.pool = ThreadPool(10)
         self.host = None
 
@@ -23,27 +23,27 @@ class Ping:
             self.host = False
 
     def ping_target(self, ip):
-        sleep(1)
         print(f"Trying to reach {ip}...")
-        request = ping(ip, timeout=self.timeout)
-        if request is None:
-            print(f"Can't reach {ip}")
-            return False
+        ping_ms = ping(ip, timeout=self.timeout)
+        if ping_ms is None:
+            self.window.ip_list[ip] = None
+            return ip, None  # Return None if IP can't be reached
         else:
-            print(f"Ping for {ip}: {request}")
-            return True
+            self.window.ip_list[ip] = ping_ms
+            return ip, ping_ms  # Return ping if IP can be reached
 
     def main(self):
         self.find_self()
         print(f"This device has IP {self.host}")
-        for ips in self.ips:
+        for ips in self.window.ip_list:
             self.pool.apply_async(self.ping_target, args=(ips, ))
 
     def await_results(self):
-        sleep(self.timeout + 1) # Give the threads some time to return the results.
+        sleep(self.timeout + 1)  # Give the threads some time to return the results.
 
 
 while True:
     ping_class = Ping()
     ping_class.main()
     ping_class.await_results()
+    ping_class.window.draw_window()
